89606bfe950e80d64d9b24e93df4a5f9731d7e71
Kai Micah Mills <kaimicahmills@gmail.com>
2021-06-25 23:32:30 -0600

    remove dan abramamamamovs retarded shit

---
+++ src/garden/garden.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+/**
+ * EDEN
+ * Authors:
+ * Kai Micah Mills
+ * Justin Benz
+ *
+ * @returns {JSX.Element}
+ * @constructor
+ */
+
+const Garden = () => {
+  return (
+    <></>
+  );
+};
+
+export default Garden;
